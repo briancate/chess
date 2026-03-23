9cc722b12ca01d315abb9736a0d2fbf90b2cd640
Brian Cate <brian.j.cate@gmail.com>
2026-03-23 17:43:55 -0600

    ChessBoard now works fully (will eventally need to be updated to accept a ChessGame object as a parameter, I assume)

--- client/src/main/java/ui/ChessBoard.java
+++ client/src/main/java/ui/ChessBoard.java
@@ -1,8 +1,5 @@
 package ui;
 
-import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
-
 import static ui.EscapeSequences.*;
 
 public class ChessBoard {
@@ -37,31 +34,28 @@ public class ChessBoard {
     private static final String[] ROW_NUMBERS_WHITE_PERSPECTIVE = {"1", "2", "3", "4", "5", "6", "7", "8"};
     private static final String[] ROW_NUMBERS_BLACK_PERSPECTIVE = {"8", "7", "6", "5", "4", "3", "2", "1"};
 
-    // use this to alternate between light and dark squares?
-    private boolean nextSquareInRowIsDark;
 
 
-
-    public static void main(String[] args) {
+    static void main() {
         // this seems unnecessary
         // var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
         System.out.print(ERASE_SCREEN);
 
-        drawHorizontalHeader("WHITE");
-        drawRowOfSquares(true, 8);
-        drawRowOfSquares(false, 7);
-        drawRowOfSquares(true, 6);
-        drawRowOfSquares(false, 5);
-        drawRowOfSquares(true, 4);
-        drawRowOfSquares(false, 3);
-        drawRowOfSquares(true, 2);
-        drawRowOfSquares(false, 1);
-        drawHorizontalHeader("WHITE");
+//        drawHorizontalHeader("WHITE");
+//        drawRowOfSquares(true, 8);
+//        drawRowOfSquares(false, 7);
+//        drawRowOfSquares(true, 6);
+//        drawRowOfSquares(false, 5);
+//        drawRowOfSquares(true, 4);
+//        drawRowOfSquares(false, 3);
+//        drawRowOfSquares(true, 2);
+//        drawRowOfSquares(false, 1);
+//        drawHorizontalHeader("WHITE");
 
-//        drawChessBoard("WHITE");
-//        System.out.println();
-//        drawChessBoard("BLACK");
+        drawChessBoard("WHITE");
+        System.out.println();
+        drawChessBoard("BLACK");
 
 
 
@@ -77,60 +71,82 @@ public class ChessBoard {
         // get the top border, and then do row by row
         if (teamColor.equals("WHITE")) {drawChessBoardWhite();}
         else {drawChessBoardBlack();}
-
-        drawHorizontalHeader(teamColor);
     }
 
     private static void drawChessBoardWhite() {
         // use a for loop int i = size; i > 0; i-- ?
         // set isDarkSquare to false ?
-        drawHorizontalHeader("WHITE");
-        System.out.print(SET_TEXT_COLOR_RED);
+        drawHorizontalBorder("WHITE");
+
+//        System.out.print(SET_TEXT_COLOR_RED);
+        boolean rowStartsWithLight = true;
         for (int i = BOARD_SIZE_IN_SQUARES; i > 0; i--) {
-            drawRowOfSquares(true, 1); // CHANGE THIS
+            drawRowOfSquares(rowStartsWithLight, i, "WHITE");
+            rowStartsWithLight = !rowStartsWithLight;
         }
 
-        drawHorizontalHeader("WHITE");
+        drawHorizontalBorder("WHITE");
     }
 
     private static void drawChessBoardBlack() {
         // use a for loop int i = 0; i < size; i++ ?
         // set isDarkSquare to true ?
-        drawHorizontalHeader("BLACK");
-        for (int i = 0; i < BOARD_SIZE_IN_SQUARES; i++) {
-            drawRowOfSquares(true, 1); // CHANGE THIS
+        drawHorizontalBorder("BLACK");
+
+        boolean rowStartsWithLight = true;
+        for (int i = 1; i <= BOARD_SIZE_IN_SQUARES; i++) {
+            drawRowOfSquares(rowStartsWithLight, i, "BLACK"); // CHANGE THIS
+            rowStartsWithLight = !rowStartsWithLight;
         }
-        drawHorizontalHeader("BLACK");
+
+        drawHorizontalBorder("BLACK");
     }
 
 
 
-    public static void drawHorizontalHeader(String teamColor) {
-        // this should make the full line of squares with border colors
-        // but, I need to find out how to reverse the order of the letters for the board from black's perspective
+    public static void drawHorizontalBorder(String teamColor) {
         System.out.print(SET_TEXT_COLOR_BLACK);
         printSquare(" ", "Gray");
-        for (String character : BORDER_ROW_WHITE_PERSPECTIVE) {
-            printSquare(character, "Gray");
+
+        if (teamColor.equals("WHITE")) {
+            for (String character : BORDER_ROW_WHITE_PERSPECTIVE) {
+                printSquare(character, "Gray");
+            }
         }
+        else {
+            for (String character : BORDER_ROW_BLACK_PERSPECTIVE) {
+                printSquare(character, "Gray");
+            }
+        }
+
         printSquare(" ", "Gray");
 
         printNewLine();
 
     }
 
-    public static void drawRowOfSquares(boolean firstSquareIsDark, int rowNumber) {
-        // let this take the row string arrays?
-        // print a border square with the row number as the text
-        // then print out 8 squares, alternating color, with text if a piece is present
-        // then print a second border square with the same row number as before
-        System.out.print(SET_TEXT_COLOR_BLACK);
-        printSquare(String.valueOf(rowNumber), "Gray");
+    public static void drawRowOfSquares(boolean firstSquareIsDark, int rowNumber, String teamColor) {
+        printBorderSquare(String.valueOf(rowNumber));
+
+        if (rowNumber == 1 || rowNumber == 2) {System.out.print(SET_TEXT_COLOR_RED);}
+        else if (rowNumber == 7 || rowNumber == 8) {System.out.print(SET_TEXT_COLOR_BLUE);}
+
         for (int i = 0; i < BOARD_SIZE_IN_SQUARES; i++) {
-            printSquare(" ", firstSquareIsDark ? "WHITE" : "BLACK");
+
+            String character;
+            if (rowNumber == 2 || rowNumber == 7) {character = "P";}
+            else if (rowNumber == 1 || rowNumber == 8) {
+                if (teamColor.equals("WHITE")) {character = ROW_ONE_OR_EIGHT_WHITE_PERSPECTIVE[i];}
+                else {character = ROW_ONE_OR_EIGHT_BLACK_PERSPECTIVE[i];}
+            }
+            else {character = " ";}
+
+
+            printSquare(character, firstSquareIsDark ? "WHITE" : "BLACK");
             firstSquareIsDark = !firstSquareIsDark;
         }
-        printSquare(String.valueOf(rowNumber), "Gray");
+
+        printBorderSquare(String.valueOf(rowNumber));
 
         printNewLine();
     }
@@ -141,6 +157,11 @@ public class ChessBoard {
         // it must be the background color
         else {setBackgroundBorderColor();}
         System.out.print(" " + character + " ");
+    }
+
+    public static void printBorderSquare(String rowNumber) {
+        System.out.print(SET_TEXT_COLOR_BLACK);
+        printSquare(rowNumber, "Gray");
     }
 
     public static void setBackgroundDark() {
